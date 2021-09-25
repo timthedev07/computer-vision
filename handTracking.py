8b1c5cfec4f19b1f5e31bb0b1430cc08a282f424
Tim <harvard90873@gmail.com>
2021-09-25 22:45:10 +0200

    recognize hands

--- handTracking.py
+++ handTracking.py
@@ -4,13 +4,14 @@ import time
 
 
 def main():
-    filename = "./assets/chain punch.mp4"
+    filename = "./assets/IP MAN 0.mp4"
     # define capture source
     cap = cv2.VideoCapture(filename)
 
     # get the hands recognition object
     mpHands = mp.solutions.hands
-    hands = mpHands.Hands()
+    hands = mpHands.Hands(False, 4, 0.65)
+    mpDraw = mp.solutions.drawing_utils
 
     frames = []
 
@@ -31,6 +32,16 @@ def main():
     cap.release()
 
     print("Finish reading video")
+
+    result = []
+
+    for frame in frames:
+        currResult = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        if currResult.multi_hand_landmarks:
+            for handLandmark in currResult.multi_hand_landmarks:
+                mpDraw.draw_landmarks(frame, handLandmark, mpHands.HAND_CONNECTIONS)
+
+    print("Finish processing hand detection")
 
     for frame in frames:
         cv2.imshow("Frame", frame)
