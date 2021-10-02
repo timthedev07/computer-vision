cc7d4855303a1a7352d5452192e2be508813d5d3
Tim <harvard90873@gmail.com>
2021-10-02 21:52:48 +0200

    detect distance between the index finger and thumb

--- src/projects/gestureVolumeControl.py
+++ src/projects/gestureVolumeControl.py
@@ -1,5 +1,6 @@
 import cv2
-from src.modules import handTracking
+from src.modules import handTracking as ht
+import math
 
 CAMERA_WIDTH, CAMERA_HEIGHT = 640, 480
 
@@ -9,8 +10,39 @@ def main():
     cap.set(3, CAMERA_WIDTH)
     cap.set(4, CAMERA_HEIGHT)
 
+    detector = ht.HandDetector(minDetectionConfidence=0.7)
+
     while True:
         success, img = cap.read()
+
+        # do stuff here
+
+        (img, hands) = detector.findHands(img)
+        if hands and len(hands) > 0:
+            hands = [hands[0]]
+            hand = hands[0]
+
+            img = detector.highlightLandmark(
+                img,
+                hands,
+                4,
+            )
+            img = detector.highlightLandmark(img, hands, 8)
+            img = detector.connectLandmarks(img, hands, 4, 8, 3)
+            _, x1, y1 = hand[4]
+            _, x2, y2 = hand[8]
+            midX, midY = (x1 + x2) // 2, (y1 + y2) // 2
+
+            length = math.hypot(x2 - x1, y2 - y1)
+
+            midCircleColor = ht.EMPHASIS_COLOR
+
+            if length <= 40:
+                midCircleColor = (255, 0, 0)
+
+            cv2.circle(img, (midX, midY), 12, midCircleColor, cv2.FILLED)
+
+        # ===========
 
         if success is False:
             break
