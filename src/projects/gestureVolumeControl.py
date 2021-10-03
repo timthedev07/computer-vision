231d5c33797139a733104c547cb9ef9751ed0d5d
Tim <harvard90873@gmail.com>
2021-10-03 09:08:12 +0200

    complete

--- src/projects/gestureVolumeControl.py
+++ src/projects/gestureVolumeControl.py
@@ -3,10 +3,14 @@ import cv2
 from src.modules import handTracking as ht
 import math
 import numpy as np
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import osascript
+
+system = platform.system()
+if system == "Windows":
+    from ctypes import cast, POINTER
+    from comtypes import CLSCTX_ALL
+    from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+elif system == "Darwin":
+    import osascript
 
 
 CAMERA_WIDTH, CAMERA_HEIGHT = 640, 480
@@ -16,15 +20,19 @@ def main():
     cap = cv2.VideoCapture(0)
     cap.set(3, CAMERA_WIDTH)
     cap.set(4, CAMERA_HEIGHT)
+    volumeBar = 400
+    volumePercentage = 0
 
     detector = ht.HandDetector(minDetectionConfidence=0.7)
-    devices = AudioUtilities.GetSpeakers()
 
-    # volume control initialization for windows
-    volume = cast(devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None), POINTER(IAudioEndpointVolume))
-    volumeRange = volume.GetVolumeRange()
-    minVolume = volumeRange[0]
-    maxVolume = volumeRange[1]
+    if system == "Windows":
+        # volume control initialization for windows
+        devices = AudioUtilities.GetSpeakers()
+        # pylint: disable=protected-access
+        volume = cast(devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None), POINTER(IAudioEndpointVolume))
+        volumeRange = volume.GetVolumeRange()
+        minVolume = volumeRange[0]
+        maxVolume = volumeRange[1]
 
     while True:
         success, img = cap.read()
@@ -50,23 +58,30 @@ def main():
             # Finger(index and thumb) distance range: 50 - 300
             length = math.hypot(x2 - x1, y2 - y1)
 
-            system = platform.system()
-
             if system == "Windows":
                 # update volume for windows
-                newVolume = np.interp(length, [50, 300], [minVolume, maxVolume])
+                newVolume = np.interp(length, [50, 250], [minVolume, maxVolume])
                 volume.SetMasterVolumeLevel(newVolume, None)
             elif system == "Darwin":
                 # update volume for mac
-                newVolume = np.interp(length, [50, 300], [0, 100])
-                osascript.osascript(f"set volume output volume {int(newVolume)}")
+                newVolume = int(np.interp(length, [50, 250], [0, 100]))
+                volumePercentage = newVolume
+                osascript.osascript(f"set volume output volume {newVolume}")
 
             midCircleColor = ht.EMPHASIS_COLOR
+
+            volumeBar = np.interp(length, [50, 250], [400, 150])
+            if system == "Windows":
+                volumePercentage = int(np.interp(length, [50, 250], [0, 100]))
 
             if length <= 40:
                 midCircleColor = (255, 0, 0)
 
             cv2.circle(img, (midX, midY), 12, midCircleColor, cv2.FILLED)
+
+        cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+        cv2.rectangle(img, (50, int(volumeBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+        cv2.putText(img, f"{int(volumePercentage)}%", (40, 450), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
 
         # ===========
 
