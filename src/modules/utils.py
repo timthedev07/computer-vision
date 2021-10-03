1f4b267514d6b43721b859129bcb11474a36f2d5
Tim <harvard90873@gmail.com>
2021-10-03 13:57:36 +0200

    take output directory name

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -84,13 +84,22 @@ def readVideo(path: str, getSize=False, withAudio=False):
     return (video, (frameWidth, frameHeight, fps)) if getSize else video
 
 
-def outputWrite(frames: list, filename: str, fileType: str, fps: int, frameShape: tuple[int, int], audio=None):
+def outputWrite(
+    frames: list,
+    filename: str,
+    fileType: str,
+    outputDirectoryName: str,
+    fps: int,
+    frameShape: tuple[int, int],
+    audio=None,
+):
     """Handles outputing video/image
 
     Args:
         frames (list): A list of frames which the output video is going to contain.
         filename (str): File name
         fileType (str): File type
+        outputDirectoryName (str): Will be plugged in to f"out/{outputDirectoryName}/..."
         fps (int): FPS
         frameShape (tuple[int, int]): (frameWidth, frameHeight)
         audio (???, optional): The ffmpeg audio. Defaults to None and thus the video would be "muted" by default.
@@ -98,19 +107,14 @@ def outputWrite(frames: list, filename: str, fileType: str, fps: int, frameShape
     Returns:
         str: The output filename
     """
-    # if not write:
-    #     for frame in frames:
-    #         cv2.imshow("Frame", frame)
-    #         cv2.waitKey(1)
-    #     return
 
     if fileType == "video":
-        outputFilename = f"out/faceMesh/{filename.split(os.sep)[-1]}"
+        outputFilename = f"out/{outputDirectoryName}/{filename.split(os.sep)[-1]}"
         if not audio:
             outputVideo = cv2.VideoWriter(outputFilename, cv2.VideoWriter_fourcc(*"MP4V"), fps, frameShape)
             outputVideo.release()
         else:
-            bufferOutputFilename = f"out/faceMesh/buffer-{filename.split(os.sep)[-1]}"
+            bufferOutputFilename = f"out/{outputDirectoryName}/buffer-{filename.split(os.sep)[-1]}"
             outputVideo = cv2.VideoWriter(bufferOutputFilename, cv2.VideoWriter_fourcc(*"MP4V"), fps, frameShape)
             for frame in frames:
                 outputVideo.write(frame)
@@ -123,7 +127,7 @@ def outputWrite(frames: list, filename: str, fileType: str, fps: int, frameShape
             os.remove(bufferOutputFilename)
 
     else:
-        outputFilename = f"out/faceMesh/{filename.split(os.sep)[-1]}"
+        outputFilename = f"out/{outputDirectoryName}/{filename.split(os.sep)[-1]}"
         cv2.imwrite(outputFilename, frames[0])
     outputFilename = outputFilename.replace("buffer-", "")
 
