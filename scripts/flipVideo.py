c6abe76971e16354cba245ce3bbdba2d6cf93b64
Tim <harvard90873@gmail.com>
2021-10-03 20:33:05 +0200

    complete function

--- scripts/flipVideo.py
+++ scripts/flipVideo.py
@@ -3,12 +3,14 @@ from sys import argv
 import cv2
 import ffmpeg
 import tempfile
+from shutil import copyfile
 
 
 def main():
     if len(argv) != 2:
         print("Usage: python flipVideo.py <path>")
-    path = argv[1]
+    path = os.path.normpath(argv[1])
+    path = path.replace(os.sep, "/")
     cap = cv2.VideoCapture(path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     frames = []
@@ -25,23 +27,26 @@ def main():
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
-    os.remove(path)
     cap.release()
 
+    filename = path.split("/")[-1] if "/" in path else path
+
     # store the video with no sound in the system temp folder
-    bufferOutputFilename = f"{tempfile.gettempdir()}/someBuffer.mp4"
+    bufferOutputFilename = f"{tempfile.gettempdir()}/{filename}"
     outputVideo = cv2.VideoWriter(bufferOutputFilename, cv2.VideoWriter_fourcc(*"MP4V"), fps, frameShape)
     for frame in frames:
         outputVideo.write(frame)
         cv2.waitKey(1)
     outputVideo.release()
 
-    # remove the existing video
-    os.remove(path)
     processedFfmpegVideo = ffmpeg.input(bufferOutputFilename)
-    # , loglevel="quiet"
-    ffmpeg.concat(processedFfmpegVideo, audio, v=1, a=1).output(path).run()
-    os.remove(bufferOutputFilename)
+    try:
+        ffmpeg.concat(processedFfmpegVideo, audio, v=1, a=1).output(path, loglevel="quiet").run()
+        os.remove(bufferOutputFilename)
+    # pylint: disable=bare-except
+    except:
+        os.remove(path)
+        copyfile(bufferOutputFilename, path)
 
 
 if __name__ == "__main__":
