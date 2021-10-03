87072b3e0fa412ccdfb701cef5f6b932b3ed5d6c
Tim <harvard90873@gmail.com>
2021-10-03 13:53:57 +0200

    bundle stuff into helper function

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,5 +1,6 @@
 import cv2
 import ffmpeg
+import os
 
 
 def checkFileType(filename: str) -> str:
@@ -81,3 +82,49 @@ def readVideo(path: str, getSize=False, withAudio=False):
     video = (frames, audio) if withAudio else frames
 
     return (video, (frameWidth, frameHeight, fps)) if getSize else video
+
+
+def outputWrite(frames: list, filename: str, fileType: str, fps: int, frameShape: tuple[int, int], audio=None):
+    """Handles outputing video/image
+
+    Args:
+        frames (list): A list of frames which the output video is going to contain.
+        filename (str): File name
+        fileType (str): File type
+        fps (int): FPS
+        frameShape (tuple[int, int]): (frameWidth, frameHeight)
+        audio (???, optional): The ffmpeg audio. Defaults to None and thus the video would be "muted" by default.
+
+    Returns:
+        str: The output filename
+    """
+    # if not write:
+    #     for frame in frames:
+    #         cv2.imshow("Frame", frame)
+    #         cv2.waitKey(1)
+    #     return
+
+    if fileType == "video":
+        outputFilename = f"out/faceMesh/{filename.split(os.sep)[-1]}"
+        if not audio:
+            outputVideo = cv2.VideoWriter(outputFilename, cv2.VideoWriter_fourcc(*"MP4V"), fps, frameShape)
+            outputVideo.release()
+        else:
+            bufferOutputFilename = f"out/faceMesh/buffer-{filename.split(os.sep)[-1]}"
+            outputVideo = cv2.VideoWriter(bufferOutputFilename, cv2.VideoWriter_fourcc(*"MP4V"), fps, frameShape)
+            for frame in frames:
+                outputVideo.write(frame)
+                cv2.waitKey(1)
+            outputVideo.release()
+
+            processedFfmpegVideo = ffmpeg.input(bufferOutputFilename)
+
+            ffmpeg.concat(processedFfmpegVideo, audio, v=1, a=1).output(outputFilename, loglevel="quiet").run()
+            os.remove(bufferOutputFilename)
+
+    else:
+        outputFilename = f"out/faceMesh/{filename.split(os.sep)[-1]}"
+        cv2.imwrite(outputFilename, frames[0])
+    outputFilename = outputFilename.replace("buffer-", "")
+
+    return outputFilename
