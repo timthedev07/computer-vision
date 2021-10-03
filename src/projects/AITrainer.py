a248378b8b523b2b693067b7ce10b9c2301fb749
Tim <harvard90873@gmail.com>
2021-10-03 21:40:58 +0200

    try to draw bar

--- src/projects/AITrainer.py
+++ src/projects/AITrainer.py
@@ -5,6 +5,9 @@ from src.modules.utils import outputWrite, readVideo, checkFileType
 from termcolor import colored
 from src.modules.poseEstimation import PoseDetector
 
+CURL_DOWNWARDS_LIMIT = 210
+CURL_UPWARDS_LIMIT = 310
+
 
 def main():
     filename = "./assets/weightLifting0.mp4"
@@ -44,7 +47,12 @@ def main():
 
             # left arm
             (frame, angle) = detector.findAndComputeAngle(frame, pose, 11, 13, 15)
-            percentage = np.interp(angle, (210, 310), (0, 100))
+            percentage = np.interp(angle, (CURL_DOWNWARDS_LIMIT, CURL_UPWARDS_LIMIT), (0, 100))
+            bar = np.interp(
+                angle,
+                (CURL_DOWNWARDS_LIMIT + 10, CURL_UPWARDS_LIMIT - 10),
+                (650, 100),
+            )
 
             # check for a curl
             if percentage == 100:
@@ -57,8 +65,16 @@ def main():
                     # if arm reaches 0% downwards
                     curlsCount += 0.5
                     direction = 0
+            elif percentage >= 60:
+                pass
 
-            cv2.putText(frame, f"Count: {int(curlsCount)}", (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 100, 100), 5)
+        # Display bar
+        cv2.rectangle(frame, (1100, 100), (1175, 650), (255, 100, 100), cv2.FILLED)
+        cv2.rectangle(frame, (1100, int(bar)), (1175, 650), (255, 100, 100), cv2.FILLED)
+        cv2.putText(frame, f"{int(percentage)}%", (1100, 75), cv2.FONT_HERSHEY_PLAIN, 5, (255, 100, 100), 5)
+
+        # Display curls count
+        cv2.putText(frame, f"Count: {int(curlsCount)}", (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 100, 100), 5)
 
         frames.append(frame)
 
