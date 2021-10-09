44c59951866a7cfd489a7872686e912b7464f0ff
Tim <harvard90873@gmail.com>
2021-10-09 18:50:15 +0200

    toggle pen/eraser

--- src/projects/virtualPainer.py
+++ src/projects/virtualPainer.py
@@ -7,6 +7,9 @@ import src.modules.handTracking as ht
 def main():
     menusDirectory = "assets/virtualPainterMenus"
     menuFilenames = os.listdir(menusDirectory)
+    menuFilenames.sort()
+    penThickness = 15
+    prevX, prevY = -1, -1
 
     menus = []
 
@@ -20,7 +23,8 @@ def main():
 
     cap = cv2.VideoCapture(0)
     cap.set(3, 1280)
-    cap.set(3, 720)
+    cap.set(4, 720)
+    color = (255, 216, 0)
 
     while True:
         success, img = cap.read()
@@ -39,11 +43,32 @@ def main():
             x2, y2 = hand[12][1:]
 
             # check which fingers are up
-            fingerStates = detector.fingersStates(hand)
+            _, fingerStates = detector.getFingersStates(hand)
 
             # if selection mode => two fingers click on an object in the menu
+            if fingerStates[1] == 1 and fingerStates[2] == 1:
+                # checking for any clicks on the pens/eraser
+                if y1 < 140:
+                    if 100 < x1 < 250:
+                        menu = menus[0]
+                        color = (255, 216, 0)
+                    elif 350 < x1 < 500:
+                        menu = menus[1]
+                        color = (0, 0, 255)
+                    elif 700 < x1 < 850:
+                        menu = menus[2]
+                        color = (0, 255, 0)
+                    elif 950 < x1 < 1200:
+                        color = (0, 0, 0)
+                        menu = menus[3]
+                cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), color, cv2.FILLED)
 
             # if drawing mode => index finger is up
+            elif fingerStates[1] == 1 and fingerStates[2] == 0:
+                cv2.circle(img, (x1, y1), 15, color, cv2.FILLED)
+                if prevX == -1 and prevY == -1:
+                    xp, yp = x1, y1
+                cv2.line(img, (prevX, prevY), (x1, y1), color, penThickness)
 
         # placing the menu
         img[0:menuHeight, 0:menuWidth] = menu
